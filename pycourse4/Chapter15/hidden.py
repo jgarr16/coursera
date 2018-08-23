4d2944d9db6f72b84c6964e4742a3ca38cd958c1
John G <jgarr16-github@yahoo.com>
2018-08-22 20:32:32 -0400

    adding pycourse4

---
+++ pycourse4/Chapter15/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "qfRcNaJoogO2I7gUbvRHmgIeu",
+            "consumer_secret": "6arJhFbPm9zMrkXjTQyO8NJzZyWzQ5xADkSsDOJZP5FxfFlLLX",
+            "token_key": "280247466-dzcZm0RvU2AX9qrgpc6FLIo86GehAzflSPFd7PGI",
+            "token_secret": "e7MwP3GdUAFt4oO8p0eO1ijmsc3mWzh6r3aqB2yeuZinj"}
